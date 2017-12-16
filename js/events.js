0a1cc9f7df8349e199954705c6ad53f0289086d6
Benjamen King <brk.3rdof3kings@gmail.com>
2017-12-16 18:54:34 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,13 @@
-//define functions here
+function getIt() {
+  $("p").on("click", () => alert("Hey!"));
+}
+
+function frameIt() {
+  $("img").on("load", () => $("img").addClass("tasty"));
+}
 
 $(document).ready(function(){
 
-// call functions here
+ 
 
 });
