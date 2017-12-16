c709295af4bbc07b1e0b3da6b38984bdd30b1614
Benjamen King <brk.3rdof3kings@gmail.com>
2017-12-16 19:26:32 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -10,11 +10,13 @@ function frameIt() {
 
 function pressIt() {
   $("form input#typing").on("keydown", (event) => {
-    if (event.which === 71) {
+    if (event.which === 71) {     // ('71' is ASCII code for 'G')
       alert("You pressed 'G'.");
     }
   });
 }
+
+
 
 $(document).ready(function(){
 
